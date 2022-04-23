86138b26a6d2ba34d271c1974a24567af5b2911b
Vladimir Pavlov <91782115+Popeye80@users.noreply.github.com>
2022-04-23 16:12:46 +0300

    111

--- js/task-05.js
+++ js/task-05.js
@@ -4,9 +4,9 @@ const output = document.querySelector('#name-output');
 input.addEventListener('input', onInputChange);
 
 function onInputChange(event) {
-  output.textContent = event.currentTarget.value;
+  output.textContent = event.currentTarget.value.trim();
 
-  if (event.currentTarget.value === '') {
+  if (event.currentTarget.value.trim() === '') {
     output.textContent = 'Anonymous';
   }
 }
